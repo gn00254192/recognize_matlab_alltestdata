ffa6b6c2faea873614fa36b76ae8c7816e07c113
gn00254192 <gn00254192@yahoo.com.tw>
2015-04-15 01:42:30 +0800

    將一張圖切割完成後,並儲存在個別的cell裡,準備計算每個切割圖裡面的pixel點總數

--- Jay_TPS/取樣點研究、測試。隨機取/re_sort.m
+++ Jay_TPS/取樣點研究、測試。隨機取/re_sort.m
@@ -1,8 +1,8 @@
 clc;
 clear;
 close all;
-cut_row=2;  %有幾列，不是切幾刀
-cut_col=2;  %有幾行
+cut_row=3;  %有幾列，不是切幾刀
+cut_col=3;  %有幾行
 pic=imread('5762529972191233.tif');
 %figure,imshow(pic);
 Long=size(pic,1);     %看影像有多長
@@ -38,16 +38,29 @@ end
 
 
 %----------------計算每個切格裡面有多少個pixel是有值的--------------------
-count=0;
-for i=1:1:540
-    for j=1:1:960
-        temp(j,i)=pic(j,i);
-        
-        if(pic(j,i)>0)
-            count=count+1;
+%每一列作完換做下一列,以3*3圖片為例,每張圖片切割順序如下
+%1   2   3
+%4   5   6
+%7   8   9
+num_row=1;
+for  h=2:1:cut_row+1
+    num_col=1;
+    for k=4:2:(cut_col+1)*2 
+        y1=1;
+        count=0;
+        for i=num_col:1:cut_temp(1,k) 
+            x1=1;
+            for j=num_row:1:cut_temp(h,1)
+                temp(x1,y1)=pic(j,i);    
+                x1=x1+1;
+            end
+        y1=y1+1;
         end
+        figure,imshow(temp);
+        save_pic_cut{h-1,(k/2)-1}=temp;
+        num_col=num_col+col_interval
     end
+    num_row=num_row+row_interval
 end
-every_pic_point_count(1,1)=count;
-figure,imshow(temp);
+
 %----------------計算每個切格裡面有多少個pixel是有值的end--------------------
