d3e65cbbbd8d5cfa17a87a137bdbfd3fa22085bd
gn00254192 <gn00254192@yahoo.com.tw>
2015-04-17 17:59:10 +0800

    切割完成,利用隨機索引取樣,皆完成(上有小bug)
    當切圖大於7時會有錯

--- Jay_TPS/取樣點研究、測試。隨機取/re_sort.m
+++ Jay_TPS/取樣點研究、測試。隨機取/re_sort.m
@@ -1,8 +1,9 @@
 clc;
 clear;
 close all;
-cut_row=3;  %有幾列，不是切幾刀
-cut_col=3;  %有幾行
+cut_row=2;  %有幾列，不是切幾刀
+cut_col=2;  %有幾行
+select_point=300;
 pic=imread('5762529972191233.tif');
 %figure,imshow(pic);
 Long=size(pic,1);     %看影像有多長
@@ -23,7 +24,7 @@ end
 for j=1:2:(cut_col+1)*2
     temp=row_interval;
     for i=2:1:cut_row+1
-        cut_temp(i,j)=temp
+        cut_temp(i,j)=temp;
         temp=temp+row_interval;
     end
 end
@@ -53,14 +54,103 @@ for  h=2:1:cut_row+1
             for j=num_row:1:cut_temp(h,1)
                 temp(x1,y1)=pic(j,i);    
                 x1=x1+1;
+                if(pic(j,i)~=0)
+                    count=count+1;
+                end
             end
         y1=y1+1;
         end
-        figure,imshow(temp);
+%        figure,imshow(temp);
         save_pic_cut{h-1,(k/2)-1}=temp;
-        num_col=num_col+col_interval
+        save_pixel_count(h-1,(k/2)-1)=count;
+        num_col=num_col+col_interval;
     end
-    num_row=num_row+row_interval
+    num_row=num_row+row_interval;
 end
 
 %----------------計算每個切格裡面有多少個pixel是有值的end--------------------
+
+%----------------測試count有沒有寫錯----------------------
+%count=0;
+%for i=1:1:1920
+%    for j=1:1:1080
+%        if(pic(i,j)~=0)
+%            count=count+1
+%        end
+%    end
+%end
+%----------------測試count有沒有寫錯end----------------------
+
+%----------------計算每張圖要取樣多少點----------------------
+total_point=sum(sum(save_pixel_count));
+float_select_point=(save_pixel_count/total_point)*select_point;
+int_select_point=floor((save_pixel_count/total_point)*select_point);
+%----------------計算每張圖要取樣多少點end----------------------
+
+%----------------------將點補滿到足夠---------------------------
+while(sum(sum(int_select_point))<select_point)
+    decimal_point=float_select_point-int_select_point;
+    temp_decimal_point=find(decimal_point==max(max(decimal_point)));
+    decimal_point(temp_decimal_point)=0;
+    int_select_point(temp_decimal_point)=int_select_point(temp_decimal_point)+1;
+end
+%----------------------將點補滿到足夠end---------------------------
+
+%--------------------------randrom_index-----------------------------
+for i=1:1:size(int_select_point,2)
+    for j=1:1:size(int_select_point,1)
+        %int_select_point(j,i)   %依矩陣索引排列出
+        random_index{j,i}=randperm(size(find(save_pic_cut{j,i}),1));
+    end    
+end
+%--------------------------randrom_end--------------------------------
+
+%-------------------------------取樣-----------------------------------
+for i=1:1:size(save_pic_cut,2)
+    for j=1:1:size(save_pic_cut,1)
+        [x,y]=find(save_pic_cut{j,i});
+        for k=1:1:int_select_point(j,i)
+            x_temp{j,i}(k,1)=x(random_index{j,i}(1,k));
+            y_temp{j,i}(k,1)=y(random_index{j,i}(1,k));
+        end
+        %figure,plot(y_temp{j,i},-x_temp{j,i},'o')
+    end
+end
+%-------------------------------取樣end-----------------------------------
+
+%----------------------------將各個取樣圖合併-------------------------------
+x=x_temp{1,1};
+y=y_temp{1,1};
+x_y_merge=[x,y];
+for i=2:1:size(save_pic_cut,2)    %處理第一列
+    x=x_temp{1,i};
+    y=y_temp{1,i}+col_interval*(i-1);
+    x_y_temp=[x,y];
+    x_y_merge=[x_y_merge;x_y_temp];
+end
+
+for i=2:1:size(save_pic_cut,1)     %處理第一行
+    x=x_temp{i,1}+row_interval*(i-1);
+    y=y_temp{i,1};
+    x_y_temp=[x,y];
+    x_y_merge=[x_y_merge;x_y_temp];
+end
+
+for i=2:1:size(save_pic_cut,2)
+    for j=2:1:size(save_pic_cut,1)
+        x=x_temp{j,i}+row_interval*(j-1);
+        y=y_temp{j,i}+col_interval*(i-1);
+        x_y_temp=[x,y];
+        x_y_merge=[x_y_merge;x_y_temp];
+    end
+end
+
+figure,plot(x_y_merge(:,2),-x_y_merge(:,1),'o')
+%x=x_temp{1,1};
+%y=y_temp{1,1};
+%x_y_merge=[x,y];
+%x=x_temp{1,2};
+%y=y_temp{1,2}+540;
+%x_y_temp=[x,y];
+%x_y_merge=[x_y_merge;x_y_temp];
+%figure,plot(x_y_merge(:,2),-x_y_merge(:,1),'o')
